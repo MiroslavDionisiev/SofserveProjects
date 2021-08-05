0dee22e22a5208a551a22d5aa18cbed7725d66d9
MiroslavDionisiev <73421736+MiroslavDionisiev@users.noreply.github.com>
2021-08-05 18:04:57 +0300

    Added tests for details view

--- LibraryManagement/LibraryManagement_Tests/HomeControllerTests.cs
+++ LibraryManagement/LibraryManagement_Tests/HomeControllerTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Xunit;
 using LibraryManagement.Models;
 using LibraryManagement.Controllers;
+using LibraryManagement.ViewModel;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
@@ -33,18 +34,31 @@ namespace LibraryManagement_Tests
         {
             // Arrange
             var mockRepoBooks = new Mock<IBookRepository>();
-            mockRepoBooks.Setup(repo => repo.GetAllBooks()).Returns(GetTestBooks());
+            Book book = new Book()
+            {
+                Id = 1,
+                Name = "Name1",
+                Author = "Author1",
+                Genre = Genres.Fiction,
+                Language = Languages.Bulgarian,
+                Description = "Description1",
+                Published = 2000,
+                FreeCopies = 1,
+                Copies = 4
+            };
+            mockRepoBooks.Setup(repo => repo.GetBook(1)).Returns(book);
             var controller = new HomeController(mockRepoBooks.Object);
 
             // Act
-            var result = controller.Details(2);
+            var result = controller.Details(1) as ViewResult;
 
             // Assert
-            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
-            Assert.Null(redirectToActionResult.ControllerName);
-            Assert.Equal("Details", redirectToActionResult.ActionName);
-            //var model = Assert.IsAssignableFrom<Book>(viewResult.ViewData.Model);
-            //Assert.Equal(2, model.Id);
+            Assert.NotNull(result);
+            Assert.Equal("Details", result.ViewName);
+            var viewModel = controller.ViewData.Model as HomeDetailsViewModel;
+            Assert.NotNull(viewModel);
+            Assert.Equal(1, viewModel.Book.Id);
+            Assert.Equal("Book details", viewModel.PageTitle);
         }
 
         private IEnumerable<Book> GetTestBooks()
