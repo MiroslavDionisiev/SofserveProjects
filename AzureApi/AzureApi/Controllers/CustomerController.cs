db172705fc89e6cf33f97a43d7ac61dbff452d87
MiroslavDionisiev <73421736+MiroslavDionisiev@users.noreply.github.com>
2021-08-18 12:47:40 +0300

    Azure API List of Customers

---
+++ AzureApi/AzureApi/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AzureApi.Controllers
+{
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private List<Customer> listCustomers;
+
+        public CustomerController()
+        {
+            this.listCustomers = new List<Customer>
+            {
+                new Customer(){ Id = 1, FirstName="Peter", LastName="Moore", Age=35},
+                new Customer(){ Id = 2, FirstName="John", LastName="Doe", Age=36},
+                new Customer(){ Id = 3, FirstName="Mary", LastName="Parker", Age=27},
+                new Customer(){ Id = 4, FirstName="Robert", LastName="Dane", Age=30},
+                new Customer(){ Id = 5, FirstName="Judith", LastName="Jones", Age=29}
+            };
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return this.listCustomers;
+        }
+
+        [HttpGet("{leftBoundarry:int}&{rightBoundarry:int}")]
+        public IEnumerable<Customer> Get(int leftBoundarry, int rightBoundarry)
+        {
+            return this.listCustomers.Where(c => c.Age >= leftBoundarry && c.Age <= rightBoundarry);
+        }
+    }
+}
---
+++ AzureApi/AzureApi/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureApi
+{
+    public class Customer
+    {
+        public int Id {get; set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Range(1, int.MaxValue)]
+        public int Age { get; set; }
+    }
+}
